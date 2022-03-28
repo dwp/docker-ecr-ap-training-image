170265394cdfefaed29136ba74a84ec26646ce7e
Camilla Scuffi <camilla.scuffi@credera.co.uk>
2022-03-28 11:24:00 +0100

    update script

--- build/tetrain.py
+++ build/tetrain.py
@@ -1,3 +1,20 @@
-import sagemaker
-from sagemaker.estimator import Estimator
-print("hellooo")
+import boto3
+import os
+import urllib.request
+
+
+bucket = os.environ["DEST_BUCKET"]
+
+
+image_url = "http://download.companieshouse.gov.uk/Accounts_Bulk_Data-2022-02-05.zip"
+response = urllib.request.urlopen(image_url)
+image = response.read()
+filename = "dnldfile.zip"
+with open(filename, "wb") as file:
+    file.write(image)
+
+s3_client = boto3.client('s3')
+
+response = s3_client.upload_file(filename, bucket, filename)
+
+
